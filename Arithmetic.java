5e31faa2ab91bf9dfb08e15bf6a22c40adf90bb4
Pavilionall <medel_alvaro@yahoo.com>
2016-05-10 19:49:55 -0500

    first push

---
+++ Arithmetic.java
@@ -0,0 +1,22 @@
+public class Arithmetic
+{
+    public static int add(int i, int j) { return i + j; }
+    public static float add(float i, float j) {return i + j; }
+    public static float add(float i, int j) {return i + j; }
+    public static float add(int i, float j) {return i + j; }
+
+    public static int mult(int i, int j) {return i * j; }
+    public static float mult(float i, float j) {return i * j; }
+    public static float mult(float i, int j) {return i * j; }
+    public static float mult(int i, float j) {return i * j; }
+
+    public static int subract(int i, int j) {return i - j; }
+    public static float subract(float i, float j) {return i - j; }
+    public static float subract(float i, int j) {return i - j; }
+    public static float subract(int i, float j) {return i - j; }
+
+    public static int div(int i, int j) {return i / j; }
+    public static float div(float i, float j) {return i / j; }
+    public static float div(float i, int j) {return i / j; }
+    public static float div(int i, float j) {return i / j; }
+}
---
+++ testfile.c
@@ -0,0 +1,21 @@
+int a, b, c;
+float x, y, z, f;
+
+int main ( ) {
+    int n, i;
+    13 + 11;
+    n = 3;
+    i = 1;
+    f = 1;
+    x = 2.2222222;
+    x = x * 3.5;
+    x = x - 1;
+    x = x / 2;
+    while (i < n) {
+        i = i + 1;
+        f = f * 7.5656;
+    }
+
+}
+
+
